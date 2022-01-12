82cb0e67806ef454ffc5da9b0ab045dd22c1eb47
mdashfaque <ashfaquemamdu666@gmail.com>
2022-01-12 18:11:03 +0530

    navbar updated

--- Front-End/blog-app/src/Components/Navbar/Navbar.js
+++ Front-End/blog-app/src/Components/Navbar/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { IoIosNotificationsOutline } from 'react-icons/io';
+import { BsSearch } from 'react-icons/bs';
+import { BsBookmarks } from 'react-icons/bs';
 import './navbar.css';
 
 const Navbar = () => {
-  const user = null;
+  const user = useSelector((state) => state.user);
+  console.log(user);
+
   return (
     <div className="blog__navbar">
       <div className="blog__navbar-links">
@@ -14,26 +20,48 @@ const Navbar = () => {
         </div>
         <div className="blog__navbar-container">
           <div className="blog__navbar-links-container">
-            <Link className="link" to="/membership">
-              Membership
-            </Link>
-            <Link className="link" to="/write">
-              Write
-            </Link>
+            {!user.email && (
+              <Link className="link" to="/membership">
+                Membership
+              </Link>
+            )}
+
+            {!user.email && (
+              <Link className="link" to="/write">
+                Write
+              </Link>
+            )}
           </div>
           <div className="blog__navbar-get-started">
-            <Link className="link" to="/signin">
-              Sign in
-            </Link>
-            <Link to="/signin">
-              <button className="get-started-btn" type="button">
-                Get started
-              </button>
-            </Link>
+            {user.email && <BsSearch></BsSearch>}
+            {user.email && <BsBookmarks></BsBookmarks>}
+            {user.email ? (
+              <IoIosNotificationsOutline
+                className="icons"
+                size={25}
+              ></IoIosNotificationsOutline>
+            ) : (
+              <Link className="link" to="/signin">
+                Sign in
+              </Link>
+            )}
+
+            {user.profilePicURL ? (
+              <img
+                className="profile-picture"
+                src={`${user.profilePicURL}`}
+                alt={`${user.name}`}
+              />
+            ) : (
+              <Link to="/signin">
+                <button className="get-started-btn" type="button">
+                  Get started
+                </button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
-      <div className="underline"></div>
     </div>
   );
 };
